1772ba9375ec2fcc56a577c741950b077d005bb3
dankato <apearmor@gmail.com>
2017-06-28 13:21:47 -0700

    put functional

--- router.js
+++ router.js
@@ -37,6 +37,27 @@ router.delete('/:id', (req, res) => {
 })
 
 //.put -> /blog-posts/:id
-
+router.put('/:id', jsonParser, (req, res) => {
+    const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
+    for(let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if(!(field in req.body)) {
+            console.error('you are missing fields');
+            return res.status(400).send(message);
+        }
+    }
+    if(req.params.id !== req.body.id) {
+        console.error(`${req.body.id} and ${req.params.id} do not match`);
+        return res.status(400).send(message);
+    }
+    const updatedItem = BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title, 
+        content: req.body.content, 
+        author: req.body.author, 
+        publishDate: req.body.publishDate
+    });
+    res.status(204).end();
+})
 
 module.exports = router;
